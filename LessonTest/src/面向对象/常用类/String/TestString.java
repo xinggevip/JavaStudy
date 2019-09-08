9fcac2e1fc7d97e2270e2706a61c4e3b92132cbc
xinggevip <1511844263@qq.com>
2019-09-08 10:20:26 +0800

    字符串编译优化

--- LessonTest/src/面向对象/常用类/String/TestString.java
+++ LessonTest/src/面向对象/常用类/String/TestString.java
@@ -1,5 +1,7 @@
 package 面向对象.常用类.String;
 
+import javax.print.attribute.standard.Chromaticity;
+
 /**
  * 什么是字符串？
  * 把字符串联在一起
@@ -12,6 +14,11 @@ package 面向对象.常用类.String;
  * 
  * 字符串本质其实是一个char数组
  * private final char value[];
+ * 
+ * 字符串的比较：
+ * 1. == 比较两个内存地址是否相等
+ * 2.使用equals 它实在Object 和 == 相同
+ * 建议子类，自己去覆盖次方法，自己在内部类当中根据自己的需求去判断两个值是否相等
  * @author xingge
  *
  */
@@ -19,9 +26,25 @@ package 面向对象.常用类.String;
 public class TestString {
 
 	public static void main(String[] args) {
+		// char c[] = {'a','b','c','d'};
+		// System.out.println(c);
 		
-		String str = "ABC";
-		System.out.println(str.length());
+		String str1 = "ABC";// 不可修改，再次复制会创建新的对象
+		String str2 = new String("ABC");
+		if (str1 == str2) {
+			System.out.println("相等");
+		}else {
+			System.out.println("不相等");
+		}
+		
+		// String类覆盖了equals
+		// 1.想去比较两个对象地址相不相等
+		// 2.如果不相等，再去判断是否为String，是 逐个断每个字符是否相等
+		if (str1.equals(str2)) {
+			System.out.println("相等");
+		}else {
+			System.out.println("不相等");
+		}
 	}
 
 }
