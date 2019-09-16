df89d05db3786a7aa6277f50ebc39219f2aaee11
xinggevip <1511844263@qq.com>
2019-09-16 21:13:36 +0800

    TreeMap

---
+++ LessonTest/src/集合/map/Lesson07.java
@@ -0,0 +1,20 @@
+package 集合.map;
+
+import java.util.TreeMap;
+
+public class Lesson07 {
+
+	public static void main(String[] args) {
+		// TreeMap是有序的
+		TreeMap<String, Integer> maps1 = new TreeMap<>();
+		maps1.put("b", 2);
+		maps1.put("a", 1);
+		maps1.put("c", 3);
+		System.out.println(maps1); // {a=1, b=2, c=3}
+		
+		
+		
+
+	}
+
+}
