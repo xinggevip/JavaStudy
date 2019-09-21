ff5e2a2c964641951c5cf7e915ad08167e80e04a
xinggevip <1511844263@qq.com>
2019-09-21 21:54:21 +0800

    ini方法参数Study

--- myproject/src/com/xing/servlet/TestServlet.java
+++ myproject/src/com/xing/servlet/TestServlet.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import javax.servlet.Servlet;
 import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
@@ -11,9 +12,11 @@ import javax.servlet.http.HttpServletResponse;
 
 public class TestServlet implements Servlet{
 
+	// servlet销毁的时候触发，服务器关闭的时候
 	@Override
 	public void destroy() {
 		// TODO Auto-generated method stub
+		System.out.println("服务器关闭了");
 		
 	}
 
@@ -29,12 +32,24 @@ public class TestServlet implements Servlet{
 		return null;
 	}
 
+	// init方法只在创建对象后执行一次
 	@Override
-	public void init(ServletConfig arg0) throws ServletException {
+	public void init(ServletConfig config) throws ServletException {
 		// TODO Auto-generated method stub
+		System.out.println("init...");
 		
+		// config 可以获取xml配置信息
+		// 获取servlet名称
+		String name = config.getServletName();
+		System.out.println(name);
+		// 获取初始化参数web.xml
+		String value = config.getInitParameter("mingzi");
+		System.out.println(value);
+		// 获取servletContext
+		ServletContext sc = config.getServletContext();
 	}
 
+	// 接收一次请求，触发一次service方法
 	@Override
 	public void service(ServletRequest arg0, ServletResponse arg1) throws ServletException, IOException {
 		// TODO Auto-generated method stub
