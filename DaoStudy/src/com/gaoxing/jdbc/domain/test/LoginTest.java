e690176955951ad0ad7c4575db1ba501fd329a4e
xinggevip <1511844263@qq.com>
2019-09-22 20:26:30 +0800

    修复SQL注入

--- DaoStudy/src/com/gaoxing/jdbc/domain/test/LoginTest.java
+++ DaoStudy/src/com/gaoxing/jdbc/domain/test/LoginTest.java
@@ -3,6 +3,7 @@ package com.gaoxing.jdbc.domain.test;
 
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -10,6 +11,7 @@ import java.sql.Statement;
 import org.junit.Test;
 
 import com.gaoxing.jdbc.util.JdbcUtil;
+import com.mysql.jdbc.JDBC4PreparedStatement;
 
 public class LoginTest {
 	
@@ -24,10 +26,14 @@ public class LoginTest {
 		// 3.ÅÐ¶Ï
 		Connection conn = JdbcUtil.getConn();
 		try {
-			Statement stt = conn.createStatement();
-			String sql = "select * from user where name ='"+name+"'and pwd = '"+pwd+"'";
-			System.out.println(sql);
-			ResultSet res = stt.executeQuery(sql);
+			
+			String sql = "select * from user where name = ? and pwd = ?";
+			PreparedStatement stt = conn.prepareStatement(sql);
+			stt.setString(1, name);
+			stt.setString(2,pwd);
+			String asSql = ((JDBC4PreparedStatement)stt).asSql();
+			System.out.println(asSql); // select * from user where name = '\' or 1=1 or\'' and pwd = '1234'
+			ResultSet res = stt.executeQuery();
 			if (res.next()) {
 				return "µÇÂ¼³É¹¦£¡";
 			}else {
