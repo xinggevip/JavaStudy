965175211c4a2b800e12a63e0bb91937e4d802f2
xinggevip <1511844263@qq.com>
2019-09-24 18:42:31 +0800

    从数据库下载图片

--- DaoStudy/src/com/gaoxing/jdbc/domain/test/BlobTest.java
+++ DaoStudy/src/com/gaoxing/jdbc/domain/test/BlobTest.java
@@ -1,8 +1,14 @@
 package com.gaoxing.jdbc.domain.test;
 
 import java.io.FileInputStream;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.sql.Blob;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import com.gaoxing.jdbc.util.JdbcUtil;
 
@@ -24,10 +30,36 @@ public class BlobTest {
 			JdbcUtil.clossAll(ps, conn);
 		}
 	}
+	// 从数据库下载图片
+	public static void test2(){
+		Connection conn = JdbcUtil.getConn();
+		String sql = "select * from student where id = ?";
+		PreparedStatement ps = null;
+		ResultSet res = null;
+		try {
+			ps = conn.prepareStatement(sql);
+			ps.setInt(1, 1);
+			res = ps.executeQuery();
+			if (res.next()) {
+				// 获取数据库图片二进制流
+				Blob blob = res.getBlob("img");
+				InputStream in = blob.getBinaryStream();
+				Files.copy(in, Paths.get("D:/mypicture.jpg"));
+				
+			}
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally {
+			JdbcUtil.clossAll(res, ps, conn);
+		}
+	}
 
 	public static void main(String[] args) {
 		// 上传图片
-		test1();
+//		test1();
+		test2();
 
 	}
 
