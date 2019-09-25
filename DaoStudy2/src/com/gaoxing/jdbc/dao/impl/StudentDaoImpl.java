4d97a44f8bc5c598ad91901bcec1c7ae852bf8d7
xinggevip <1511844263@qq.com>
2019-09-25 13:25:55 +0800

    DML代码抽取

---
+++ DaoStudy2/src/com/gaoxing/jdbc/dao/impl/StudentDaoImpl.java
@@ -0,0 +1,123 @@
+package com.gaoxing.jdbc.dao.impl;
+
+import java.sql.PreparedStatement;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.gaoxing.jdbc.dao.IStudentDao;
+import com.gaoxing.jdbc.domain.Student;
+import com.gaoxing.jdbc.util.JdbcUtil;
+import com.mysql.jdbc.JDBC4PreparedStatement;
+
+public class StudentDaoImpl implements IStudentDao{
+
+	@Override
+	public void save(Student stu) {
+		
+		String sql = "insert into student (name,age) value(?,?)";
+		this.executeUpdate(sql, stu.getName(),stu.getAge());
+		
+	}
+	
+	public static int executeUpdate(String sql,Object...params) {
+		Connection conn = JdbcUtil.getConn();
+		PreparedStatement stt = null;
+		try {
+			stt = conn.prepareStatement(sql);
+			// ±éÀú²ÎÊý
+			for(int i = 0;i < params.length;i++) {
+				stt.setObject(i+1, params[i]);
+			}
+			
+			return stt.executeUpdate();
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally {
+			JdbcUtil.clossAll(stt, conn);
+		}
+		return 0;
+	}
+
+	@Override
+	public void update(int id,Student stu) {
+		
+		String sql = "update student set name = ?,age = ? where id = ?";
+		this.executeUpdate(sql, stu.getName(),stu.getAge(),id);
+		
+	}
+
+	@Override
+	public void delete(Integer id) {
+		
+		String sql = "delete from student where id = ?";
+		this.executeUpdate(sql, id);
+		
+	}
+
+	@Override
+	public Student get(int id) {
+		Connection conn = JdbcUtil.getConn();
+		String sql = "select * from student where id = ?";
+		PreparedStatement stt = null;
+		ResultSet res = null;
+		try {
+			stt = conn.prepareStatement(sql);
+			stt.setInt(1, id);
+			String asSql = ((JDBC4PreparedStatement)stt).asSql();
+			System.out.println(asSql);
+			res = stt.executeQuery();
+			if(res.next()) {
+				Student stu = new Student();
+				stu.setId(res.getInt("id"));
+				stu.setName(res.getString("name"));
+				stu.setAge(res.getInt("age"));
+				return stu;
+			}
+			
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally {
+			JdbcUtil.clossAll(res, stt, conn);
+		}
+		return null;
+	}
+
+	@Override
+	public List<Student> getAll() {
+		Connection conn = JdbcUtil.getConn();
+		String sql = "select * from student";
+		PreparedStatement stt = null;
+		ResultSet res = null;
+		List<Student> list = new ArrayList<Student>();
+		try {
+			stt = conn.prepareStatement(sql);
+			res = stt.executeQuery();
+			while(res.next()) {
+				Student stu = new Student();
+				stu.setId(res.getInt("id"));
+				stu.setName(res.getString("name"));
+				stu.setAge(res.getInt("age"));
+				list.add(stu);
+			}
+			return list;
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally {
+			JdbcUtil.clossAll(res, stt, conn);
+		}
+		return null;
+	}
+
+
+	
+
+}
---
+++ DaoStudy2/src/com/gaoxing/jdbc/util/JdbcUtil.java
@@ -0,0 +1,85 @@
+package com.gaoxing.jdbc.util;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.security.spec.DSAGenParameterSpec;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+
+import com.alibaba.druid.pool.DruidDataSourceFactory;
+
+public class JdbcUtil {
+
+	public static DataSource ds = null;
+	static {
+		// ≈‰÷√¡¨Ω”≥ÿ
+		try {
+			Properties p = new Properties();
+			FileInputStream in = new FileInputStream("resource/db.properties");
+			p.load(in);
+//			ds = BasicDataSourceFactory.createDataSource(p);
+			ds = DruidDataSourceFactory.createDataSource(p);
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+
+	public static Connection getConn() {
+		try {
+			return ds.getConnection();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	public static void clossAll( ResultSet res, Statement stt,Connection conn) {
+
+		try {
+			if (res != null) {
+				res.close();
+			}
+			if (stt != null) {
+				stt.close();
+			}
+			if (conn != null) {
+				conn.close();
+			}
+	
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+	
+	
+	public static void clossAll(Statement stt,Connection conn) {
+
+		try {
+			if (stt != null) {
+				stt.close();
+			}
+			if (conn != null) {
+				conn.close();
+			}
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+}
