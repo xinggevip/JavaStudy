c32e801be96a968ee5cae1e78afc63715c5e8bb7
xinggevip <1511844263@qq.com>
2019-09-27 12:10:20 +0800

    301重定向

--- WebProject2/src/com/gaoxing/response/ResponseServlet.java
+++ WebProject2/src/com/gaoxing/response/ResponseServlet.java
@@ -18,7 +18,7 @@ public class ResponseServlet extends HttpServlet {
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// 设置响应头，响应行，相应体
 		// 设置状态码
-		response.setStatus(200);
+		response.setStatus(301);
 		System.out.println("service----------");
 		
 		// 添加响应头
@@ -29,6 +29,17 @@ public class ResponseServlet extends HttpServlet {
 		// 设置响应头  能修改自己添加的也能修改默认的
 		response.setHeader("name", "Mr Gao");
 		response.addIntHeader("age", 22);
+		
+		// 301重定向
+		// 通过设置响应头
+//		response.setHeader("location", "/WebProject2/reloca");
+		// 通过封装的方法
+//		response.sendRedirect("/WebProject2/reloca");
+		// 定时刷新重定向 隔3秒再重定向
+//		response.setHeader("refresh", "3;url=/WebProject2/reloca");
+		response.setHeader("refresh", "3;url=https://www.baidu.com");
+		
+		
 		
 		
 	}
