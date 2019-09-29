1f549a18bab90b3f623b4b3b4d699424edf5e747
xinggevip <1511844263@qq.com>
2019-09-29 12:18:52 +0800

    创建cookie并响应

---
+++ CookSession/src/com/gaoxing/cookie/CookieServlet.java
@@ -0,0 +1,22 @@
+package com.gaoxing.cookie;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/CookieServlet")
+public class CookieServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// 1.创建cookie
+		Cookie cookie = new Cookie("key", "value");
+		// 2.返回给response
+		response.addCookie(cookie);
+	}
+
+}
