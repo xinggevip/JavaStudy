92b4a3ffad5819e4e2ac358af1dba726166b9dd9
xinggevip <1511844263@qq.com>
2019-09-29 12:34:09 +0800

    设置cookie持久化时间

--- CookSession/src/com/gaoxing/cookie/CookieServlet.java
+++ CookSession/src/com/gaoxing/cookie/CookieServlet.java
@@ -15,6 +15,11 @@ public class CookieServlet extends HttpServlet {
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// 1.创建cookie
 		Cookie cookie = new Cookie("key", "value");
+		/**
+		 * 默认情况cookie是会话级别的
+		 * 打开浏览器，关闭浏览器为一次会话
+		 * 如果不设置持久化时间，cookie会存储再浏览器内存中，浏览器关闭，cookie消失
+		 */
 		// 2.返回给response
 		response.addCookie(cookie);
 	}
