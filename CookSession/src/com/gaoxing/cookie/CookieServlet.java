933589937c35cfedeb0a8242a85f3426005517fd
xinggevip <1511844263@qq.com>
2019-09-29 12:51:28 +0800

    设置cookie携带路径

--- CookSession/src/com/gaoxing/cookie/CookieServlet.java
+++ CookSession/src/com/gaoxing/cookie/CookieServlet.java
@@ -19,7 +19,19 @@ public class CookieServlet extends HttpServlet {
 		 * 默认情况cookie是会话级别的
 		 * 打开浏览器，关闭浏览器为一次会话
 		 * 如果不设置持久化时间，cookie会存储再浏览器内存中，浏览器关闭，cookie消失
+		 * 
+		 * 默认情况下会访问创建cookie的web资源相同的路径 （http://localhost:8080/CookSession/）
+		 * http://localhost:8080/CookSession/CookieServlet1
+		 * http://localhost:8080/CookSession/CookieServlet2
+		 * http://localhost:8080/CookSession/CookieServlet3
 		 */
+		// 设置cookie携带路径
+		cookie.setPath("/CookSession"); // 访问指定工程都携带cookie信息
+		//cookie.setPath("/"); // 访问所有工程都携带cookie信息
+		//cookie.setPath("/CookSession/CookieServlet"); // 访问工程下的指定servlet才下带cookie信息
+		
+		// 设置cookie在客户端存储时间
+		cookie.setMaxAge(60);
 		// 2.返回给response
 		response.addCookie(cookie);
 	}
