9e5eab99fe9316e18d11fd63fa2d841146d5f4e8
xinggevip <1511844263@qq.com>
2019-09-30 16:24:12 +0800

    MVC模式后台管理商品列表业务

---
+++ MyStoreAdmin/src/com/gaoxing/service/GoodsService.java
@@ -0,0 +1,17 @@
+package com.gaoxing.service;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import com.gaoxing.dao.GoodsDao;
+import com.gaoxing.domain.Goods;
+
+public class GoodsService {
+
+	public List<Goods> getAllGoods() throws SQLException {
+		GoodsDao goodsDao = new GoodsDao();
+		List<Goods> allGoods = goodsDao.getAllGoods();
+		return allGoods;
+	}
+
+}
---
+++ MyStoreAdmin/src/com/gaoxing/web/GoodsListServlet.java
@@ -0,0 +1,33 @@
+package com.gaoxing.web;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.gaoxing.domain.Goods;
+import com.gaoxing.service.GoodsService;
+
+@WebServlet("/GoodsListServlet")
+public class GoodsListServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		GoodsService goodsService = new GoodsService();
+		try {
+			List<Goods> allGoods = goodsService.getAllGoods();
+			System.out.println(allGoods);
+			request.setAttribute("allGoods", allGoods);
+			request.getRequestDispatcher("admin/main.jsp").forward(request, response);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
